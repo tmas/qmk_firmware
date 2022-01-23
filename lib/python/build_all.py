0c86497ae7d6fc942fe5087415bf2f4270d0962d
Thomas Lord <thomas@lord.dev>
2022-01-22 19:42:13 -0500

    Fix indents

--- lib/python/build_all.py
+++ lib/python/build_all.py
@@ -19,25 +19,25 @@ ret = subprocess.run(command, capture_output=True, shell=True)
 BOARDS = ret.stdout.decode().split('\n')
 
 if args.debug:
-  print ("using debug mode")
+    print ("using debug mode")
 
 if args.whitelist:
-  # Grab the list of whitelisted keyboards
-  whitelisted_kb_command = "cat " + args.whitelist + " | tr -d '\r'"
-  whitelisted_kb_ret = subprocess.run(whitelisted_kb_command, capture_output=True, shell=True)
-  WHITELISTED_BOARDS = whitelisted_kb_ret.stdout.decode().split('\n')
-  if args.debug:
-    print ("using whitelist: ", args.whitelist)
-    print ("whitelist contents: ", WHITELISTED_BOARDS)
+    # Grab the list of whitelisted keyboards
+    whitelisted_kb_command = "cat " + args.whitelist + " | tr -d '\r'"
+    whitelisted_kb_ret = subprocess.run(whitelisted_kb_command, capture_output=True, shell=True)
+    WHITELISTED_BOARDS = whitelisted_kb_ret.stdout.decode().split('\n')
+    if args.debug:
+        print ("using whitelist: ", args.whitelist)
+        print ("whitelist contents: ", WHITELISTED_BOARDS)
 
 if args.blacklist:
-  # Grab the list of blacklisted keyboards
-  blacklisted_kb_command = "cat " + args.blacklist + " | tr -d '\r'"
-  blacklisted_kb_ret = subprocess.run(blacklisted_kb_command, capture_output=True, shell=True)
-  BLACKLISTED_BOARDS = blacklisted_kb_ret.stdout.decode().split('\n')
-  if args.debug:
-    print ("using blacklist: ", args.blacklist)
-    print ("blacklist contents: ", BLACKLISTED_BOARDS)
+    # Grab the list of blacklisted keyboards
+    blacklisted_kb_command = "cat " + args.blacklist + " | tr -d '\r'"
+    blacklisted_kb_ret = subprocess.run(blacklisted_kb_command, capture_output=True, shell=True)
+    BLACKLISTED_BOARDS = blacklisted_kb_ret.stdout.decode().split('\n')
+    if args.debug:
+        print ("using blacklist: ", args.blacklist)
+        print ("blacklist contents: ", BLACKLISTED_BOARDS)
 
 def main():
     for line in BOARDS:
@@ -58,21 +58,21 @@ def main():
       print ('Filtered and processed boards: ', KEYBOARDS)
 
 def should_include(keyboard):
-  if keyboard.strip() == "":
-    return False
-  if keyboard.strip() == "lib/python/build_all.py":
-    return False
-  if args.blacklist:
-    if keyboard.strip() in BLACKLISTED_BOARDS:
-      if args.debug:
-        print ("Skipping blacklisted keyboard: ", keyboard.strip())
-      return False
-  if args.whitelist:
-    if keyboard.strip() not in WHITELISTED_BOARDS:
-      if args.debug:
-        print ("Skipping non-whitelisted keyboard: ", keyboard.strip())
-      return False
-  return True
+    if keyboard.strip() == "":
+        return False
+    if keyboard.strip() == "lib/python/build_all.py":
+        return False
+    if args.blacklist:
+        if keyboard.strip() in BLACKLISTED_BOARDS:
+        if args.debug:
+            print ("Skipping blacklisted keyboard: ", keyboard.strip())
+        return False
+    if args.whitelist:
+        if keyboard.strip() not in WHITELISTED_BOARDS:
+        if args.debug:
+            print ("Skipping non-whitelisted keyboard: ", keyboard.strip())
+        return False
+    return True
 
 if __name__ == '__main__':
     main()
